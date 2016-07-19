72933b87c67e59add9dfae3f7523a9e81c034d98
Mark Ettema <markettema@outlook.com>
2016-07-19 08:41:54 +0200

    Refactored for loop to map.

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -14,71 +14,69 @@ var GildedRose = function () {
 };
 
 
-function updateItemQuality(items, i) {
-    if (AGED_BRIE != items[i].name && BACKSTAGE_PASSES != items[i].name) {
-        if (items[i].quality > 0) {
-            if (SULFURAS != items[i].name) {
-                items[i].quality = items[i].quality - 1
+function updateItemQuality(item) {
+    if (AGED_BRIE != item.name && BACKSTAGE_PASSES != item.name) {
+        if (item.quality > 0) {
+            if (SULFURAS != item.name) {
+                item.quality = item.quality - 1
             }
         }
     } else {
-        if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1
-            if (AGED_BRIE == items[i].name) {
-                if (items[i].sellIn < 6) {
-                    items[i].quality = items[i].quality + 1
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+            if (AGED_BRIE == item.name) {
+                if (item.sellIn < 6) {
+                    item.quality = item.quality + 1
                 }
             }
             //Increases the Quality of the stinky cheese if its 11 days to due date.
-            if (AGED_BRIE == items[i].name) {
-                if (items[i].sellIn < 11) {
-                    items[i].quality = items[i].quality + 1
+            if (AGED_BRIE == item.name) {
+                if (item.sellIn < 11) {
+                    item.quality = item.quality + 1
                 }
             }
-            if (BACKSTAGE_PASSES == items[i].name) {
-                if (items[i].sellIn < 11) {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+            if (BACKSTAGE_PASSES == item.name) {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
                     }
                 }
                 //Increases the Quality of Backstage Passes if the Quality is 6 or less.
-                if (items[i].sellIn < 6) {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
                     }
                 }
             }
         }
     }
-    if (SULFURAS != items[i].name) {
-        items[i].sellIn = items[i].sellIn - 1;
+    if (SULFURAS != item.name) {
+        item.sellIn = item.sellIn - 1;
     }
-    if (items[i].sellIn < 0) {
-        if (AGED_BRIE != items[i].name) {
-            if (BACKSTAGE_PASSES != items[i].name) {
-                if (items[i].quality > 0) {
+    if (item.sellIn < 0) {
+        if (AGED_BRIE != item.name) {
+            if (BACKSTAGE_PASSES != item.name) {
+                if (item.quality > 0) {
 
-                    if (SULFURAS != items[i].name) {
-                        items[i].quality = items[i].quality - 1
+                    if (SULFURAS != item.name) {
+                        item.quality = item.quality - 1
                     }
                 }
             } else {
-                items[i].quality = items[i].quality - items[i].quality
+                item.quality = item.quality - item.quality
             }
         } else {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
             }
-            if (AGED_BRIE == items[i].name && items[i].sellIn <= 0)
-                items[i].quality = 0;
+            if (AGED_BRIE == item.name && item.sellIn <= 0)
+                item.quality = 0;
         }
     }
-    if (SULFURAS != items[i].name)
-        if (items[i].quality > 50) items[i].quality = 50;
+    if (SULFURAS != item.name)
+        if (item.quality > 50) item.quality = 50;
+    return item;
 }
 GildedRose.updateQuality = function (items) {
-    for (var i = 0; i < items.length; i++) {
-        updateItemQuality(items, i);
-    }
-    return items;
+    return items.map(updateItemQuality);
 };
