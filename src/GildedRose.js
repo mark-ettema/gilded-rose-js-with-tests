88e991fb8e124f48426260ba8dbad3481be73e90
Mark Ettema <markettema@outlook.com>
2016-07-19 08:32:05 +0200

    Extracted method updateItemQuality().

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -14,68 +14,71 @@ var GildedRose = function () {
 };
 
 
-GildedRose.updateQuality = function (items) {
-    for (var i = 0; i < items.length; i++) {
-        if (AGED_BRIE != items[i].name && BACKSTAGE_PASSES != items[i].name) {
-            if (items[i].quality > 0) {
-                if (SULFURAS != items[i].name) {
-                    items[i].quality = items[i].quality - 1
+function updateItemQuality(items, i) {
+    if (AGED_BRIE != items[i].name && BACKSTAGE_PASSES != items[i].name) {
+        if (items[i].quality > 0) {
+            if (SULFURAS != items[i].name) {
+                items[i].quality = items[i].quality - 1
+            }
+        }
+    } else {
+        if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1
+            if (AGED_BRIE == items[i].name) {
+                if (items[i].sellIn < 6) {
+                    items[i].quality = items[i].quality + 1
                 }
+            }
+            //Increases the Quality of the stinky cheese if its 11 days to due date.
+            if (AGED_BRIE == items[i].name) {
+                if (items[i].sellIn < 11) {
+                    items[i].quality = items[i].quality + 1
+                }
+            }
+            if (BACKSTAGE_PASSES == items[i].name) {
+                if (items[i].sellIn < 11) {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1
+                    }
+                }
+                //Increases the Quality of Backstage Passes if the Quality is 6 or less.
+                if (items[i].sellIn < 6) {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1
+                    }
+                }
+            }
+        }
+    }
+    if (SULFURAS != items[i].name) {
+        items[i].sellIn = items[i].sellIn - 1;
+    }
+    if (items[i].sellIn < 0) {
+        if (AGED_BRIE != items[i].name) {
+            if (BACKSTAGE_PASSES != items[i].name) {
+                if (items[i].quality > 0) {
+
+                    if (SULFURAS != items[i].name) {
+                        items[i].quality = items[i].quality - 1
+                    }
+                }
+            } else {
+                items[i].quality = items[i].quality - items[i].quality
             }
         } else {
             if (items[i].quality < 50) {
                 items[i].quality = items[i].quality + 1
-                if (AGED_BRIE == items[i].name) {
-                    if (items[i].sellIn < 6) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-                //Increases the Quality of the stinky cheese if its 11 days to due date.
-                if (AGED_BRIE == items[i].name) {
-                    if (items[i].sellIn < 11) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-                if (BACKSTAGE_PASSES== items[i].name) {
-                    if (items[i].sellIn < 11) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1
-                        }
-                    }
-                    //Increases the Quality of Backstage Passes if the Quality is 6 or less.
-                    if (items[i].sellIn < 6) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1
-                        }
-                    }
-                }
             }
+            if (AGED_BRIE == items[i].name && items[i].sellIn <= 0)
+                items[i].quality = 0;
         }
-        if (SULFURAS != items[i].name) {
-            items[i].sellIn = items[i].sellIn - 1;
-        }
-        if (items[i].sellIn < 0) {
-            if (AGED_BRIE != items[i].name) {
-                if (BACKSTAGE_PASSES!= items[i].name) {
-                    if (items[i].quality > 0) {
-                        
-                        if (SULFURAS != items[i].name) {
-                            items[i].quality = items[i].quality - 1
-                        }
-                    }
-                } else {
-                    items[i].quality = items[i].quality - items[i].quality
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-                }
-                if (AGED_BRIE == items[i].name && items[i].sellIn <= 0)
-                    items[i].quality = 0;
-            }
-        }
-        if (SULFURAS != items[i].name)
-            if (items[i].quality > 50) items[i].quality = 50;
+    }
+    if (SULFURAS != items[i].name)
+        if (items[i].quality > 50) items[i].quality = 50;
+}
+GildedRose.updateQuality = function (items) {
+    for (var i = 0; i < items.length; i++) {
+        updateItemQuality(items, i);
     }
     return items;
 };
